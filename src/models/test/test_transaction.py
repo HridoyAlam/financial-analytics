485c54c7e7d89025da00d92a0d4f9169ea734af5
HridoyAlam <optimus5289@gmail.com>
2026-09-09 18:15:19 +0600

    transaction created

---
+++ src/models/test/test_transaction.py
@@ -0,0 +1,83 @@
+from transaction import Transaction
+from stock import Stock
+from position import Position
+from portfolio import Portfolio
+import pytest
+@pytest.fixture
+def asset():
+    return Stock(
+        "AAPL",
+        "Apple Inc.",
+        [100, 105, 110],
+        "Technology",
+        2.0
+    )
+
+@pytest.fixture
+def position(asset):
+    return Position(
+            asset,
+            quantity=100,
+            average_cost=200
+    )
+@pytest.fixture
+def portfolio():
+    return Portfolio(
+            "My Portfolio",
+            30000
+        )
+@pytest.mark.parametrize("param", ["buy", "Buy", "BUY"])
+def test_valid_buy_transaction(asset, param):
+    transaction = Transaction(
+                asset,
+                param,
+                20,
+                500
+            )
+    assert transaction.transaction_type == "BUY"
+    assert transaction.quantity == 20
+    assert transaction.price == 500
+@pytest.mark.parametrize("param", ["sell", "SELL", "Sell"])
+def test_valid_sell_transaction(asset, param):
+    transaction = Transaction(
+                asset,
+                param,
+                20,
+                500
+            )
+    assert transaction.transaction_type == "SELL"
+    
+
+def test_invalid_quantity_buy_transaction(asset):
+    with pytest.raises(
+        ValueError,
+        match="Quantity must be greater than zero"
+    ):
+        Transaction(
+            asset,
+            "BUY",
+            -20,
+            500
+        )
+def test_invalid_price_sell_transaction(asset):
+    with pytest.raises(
+        ValueError,
+        match="Price must be greater than zero"
+    ):
+        Transaction(
+            asset,
+            "sell",
+            20,
+            -500
+        )
+
+
+def test_total_value(asset):
+    transaction = Transaction(
+        asset,
+        "BUY",
+        50,
+        100
+        )
+
+    assert transaction.total_value() == pytest.approx(5000)
---
+++ src/models/transaction.py
@@ -0,0 +1,45 @@
+from asset import Asset
+class Transaction():
+    def __init__(
+            self, 
+            asset:Asset, 
+            transaction_type: str, 
+            quantity: float,
+            price: float
+            ) -> None:
+
+        if not isinstance(asset, Asset):
+            raise TypeError("asset must be an instance of Asset")
+
+        if transaction_type.upper() not in ["BUY", "SELL"]:
+            raise ValueError("Transaction type must either BUY or SELL")
+
+        if quantity <= 0:
+            raise ValueError("Quantity must be greater than zero")
+        
+        if price <= 0:
+            raise ValueError("Price must be greater than zero")
+
+        self._asset = asset
+        self._transaction_type = transaction_type.upper()
+        self._quantity = quantity
+        self._price = price
+
+    @property
+    def asset(self) -> Asset:
+        return self._asset
+    
+    @property
+    def transaction_type(self) -> str:
+        return self._transaction_type
+    
+    @property
+    def quantity(self) -> float:
+        return self._quantity
+    
+    @property
+    def price(self) -> float:
+        return self._price
+
+    def total_value(self) -> float:
+        return self.quantity * self.price
